7fbd4c5910218d325cc7cacbf60cc7538032b4ec
Chu7641 <chuquangsang7641@gmail.com>
2020-12-04 21:46:11 +0700

    btvn b06

--- js/script.js
+++ js/script.js
@@ -1,49 +1,21 @@
-
-chuyenTiep = () => {
-    let web = document.getElementById("web_txt").value;
-    //console.log(web)
-    switch (web) {
-        case '1':
-            window.open("https://quantrimang.com");
-            break;
-        case '2':
-            window.open("https://download.com.vn/");
-            break;
-        case '3':
-            window.open("https://vndoc.com/")
-            break;
-        default:
-            window.open("https://meta.vn/")
-           break;
-    }
+tinhTong = () => {
+    let mang = [1,3,5,7,9];
+    //phanTu = document.getElementById('mang').value;
+   // mang.push(phanTu);
+     console.log(mang);
+    let sum = 0;
+    mang.forEach(e => {
+        sum += e;
+    });
+    alert("Tổng các phần tử trong mảng là: "+ sum)
 }
-doiMau=()=>{
-    let web = document.getElementById("web_txt").value;
-    //console.log(web)
-    switch (web) {
-        case '1':
-            document.getElementById('web1').style.color="red";
-            document.getElementById('web2').style.color="black";
-            document.getElementById('web3').style.color="black";
-            document.getElementById('web4').style.color="black";
-            break;
-        case '2':
-            document.getElementById('web1').style.color="black";
-            document.getElementById('web2').style.color="red";
-            document.getElementById('web3').style.color="black";
-            document.getElementById('web4').style.color="black";
-            break;
-        case '3':
-            document.getElementById('web1').style.color="black";
-            document.getElementById('web2').style.color="black";
-            document.getElementById('web3').style.color="red";
-            document.getElementById('web4').style.color="black";
-            break;
-        default:
-            document.getElementById('web1').style.color="black";
-            document.getElementById('web2').style.color="black";
-            document.getElementById('web3').style.color="black";
-            document.getElementById('web4').style.color="red";
-           break;
-    }
+testDom=()=>{
+    let hello=document.getElementById('hello_txt');
+    hello.outerHTML=('<h2 id="hello_txt" style="color: red;">Hello2</h2>')
+    
+}
+testBack=()=>{
+    let hello=document.getElementById('hello_txt');
+    hello.outerHTML=('<h1 id="hello_txt">Hello</h1>')
+    
 }
